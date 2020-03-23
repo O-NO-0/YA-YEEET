e10bbd39449abe85be6655e52ca907104f484ece
O-NO-0 <rooshmapraseej@gmail.com>
2020-03-23 19:35:53 +0300

    u

--- sketch.js
+++ sketch.js
@@ -1,46 +1,18 @@
-var box,database;
-var position;
-
+var database
+var gameState  = 0;
+var playerCount ;
+var form,player,game;
 function setup(){
     database = firebase.database();
     createCanvas(400,400);
-    box = createSprite(200,200,10,10);
-    box.shapeColor = "red";
+    game = new Game ();
+    game.getState();
+    game.start();
 
-    var boxpos = database.ref('ball/position');
-    boxpos.on("value",readP,showERROR);
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        Wposition(-3,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        Wposition(3,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        Wposition(0,-3);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        Wposition(0,+3);
-    }
-    drawSprites();
+   
 }
 
-function Wposition(x,y){
-database.ref('ball/position').set({
-'x':position.x+x, 
-'y':position.y+y   
-})    
-}
-function readP(data){
-position = data.val();
-box.x = position.x;
-box.y = position.y;
-}
-function showERROR(){
-    console.log("dammit");
-
-
-}
